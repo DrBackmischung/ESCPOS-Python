f5a99cb5afb87371ea1151720a0cd6d7694082e3
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-04-12 12:13:58 +0200

    Add files via upload

---
+++ escpos/pos_document.py
@@ -0,0 +1,41 @@
+from src.pos_component import POSComponent
+from src.constants import POSCommand
+from src.pos_text import POSTextBuilder
+from src.constants import POSPrintStyle, POSTextAlignment
+from src.pos_linefeed import POSLineFeed
+
+class POSDocument(POSComponent):
+    def __init__(self):
+        self.components = []
+
+    def add_component(self, component: POSComponent):
+        self.components.append(component)
+
+    def add_line_feed(self, count: int = 1):
+        self.components.append(POSLineFeed(count))
+
+    def to_bytes(self) -> bytes:
+        buffer = bytearray()
+
+        # Padding hack
+        filler = POSTextBuilder("").set_style(
+            POSPrintStyle.BOLD, POSPrintStyle.DOUBLE_WIDTH
+        ).set_alignment(
+            POSTextAlignment.CENTER
+        ).build().to_bytes()
+
+        for _ in range(4):
+            buffer += filler
+
+        buffer += bytes([POSCommand.ESC, POSCommand.PAGE_MODE])
+        buffer += bytes([POSCommand.ESC, POSCommand.PRINTER_RESET])
+        buffer += bytes([POSCommand.GS, POSCommand.STATUS_REQUEST, 1])
+        buffer += bytes([POSCommand.ESC, POSCommand.UNIDIRECTIONAL_MODE, 1])
+        buffer += bytes([POSCommand.ESC, POSCommand.PRINT_PAGE_MODE])
+
+        for comp in self.components:
+            buffer += comp.to_bytes()
+
+        buffer += bytes([POSCommand.ESC, POSCommand.PRINTER_RESET])
+
+        return bytes(buffer)
---
+++ escpos/pos_linefeed.py
@@ -0,0 +1,9 @@
+from src.pos_component import POSComponent
+from src.constants import POSCommand
+
+class POSLineFeed(POSComponent):
+    def __init__(self, count: int = 1):
+        self.count = count
+
+    def to_bytes(self) -> bytes:
+        return bytes([POSCommand.LINE_FEED] * self.count)
